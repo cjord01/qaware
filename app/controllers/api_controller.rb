cd3e5291e518ea89775e5e937b0b2d9de6d9e563
rickydo <g3tdo@yahoo.com>
2014-09-25 12:51:55 -0500

    have the json part working

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,13 +1,25 @@
 class ApiController < ApplicationController
 
   def hello
-    respond_to do |format|
-      format.json do
-        render :json => {
-          :message => "yippy kai ay motherfather!",
-          :html => "<b>whatthisbe</b>"
-        }.to_json
-      end
-    end
+    # respond_to do |format|
+    #   format.json do
+    #     render :json => {
+    #       :message => "yippy kai ay motherfather!",
+    #       :html => "<b>whatthisbe</b>"
+    #     }.to_json
+    #   end
+    # end
+    @forms = {
+      form1: "this is form 1",
+      form2: "this is form 2",
+      form3: "this is form 3"
+    }
+    @string = "yippy kai yay motherfather"
+    render :json => @forms.to_json   
+ # respond_to do |format|
+    #   format.html 
+    #   format.json { render @string  }
+    #   format.json 
+    # end
   end
 end
