6cc5a11f6672fb29a3abedec6b35a9e11268a525
Clayton Jordan <clayjchicago@gmail.com>
2014-09-26 10:54:33 -0500

    changed associations

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -1,6 +1,6 @@
 class Manager < ActiveRecord::Base
   has_many :employees
-  has_many :completed_forms
+  has_many :completed_forms, through: :employees
   has_many :forms
   has_secure_password
 end
