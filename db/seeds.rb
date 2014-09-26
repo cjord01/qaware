1ce8333c115eedd0af259243f3713d34896b14ae
rickydo <g3tdo@yahoo.com>
2014-09-26 14:12:59 -0500

    working on seed data for d3. have questions and employees created, responses in progresss

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,33 +5,68 @@ Response.delete_all
 Manager.delete_all
 Employee.delete_all
 
-manager = Manager.create(name: "admin", email: "qaware@gmail.com", password: "password", password_confirmation: "password")
+manager = Manager.create(name: "rick", email: "rick@gmail.com", password: "password", password_confirmation: "password")
 
-mike = Employee.create(name: "Mike Baker", email: Faker::Internet.email, manager_id: manager.id)
-matt = Employee.create(name: "Matt Busch", email: Faker::Internet.email, manager_id: manager.id)
-10.times do
-  Employee.create(name: Faker::Name.name, email:Faker::Internet.email, manager_id: manager.id)
+# mike = Employee.create(name: "Mike Baker", email: Faker::Internet.email, manager_id: manager.id)
+# matt = Employee.create(name: "Matt Busch", email: Faker::Internet.email, manager_id: manager.id)
+
+employees = []
+
+5.times do
+  employees << Employee.create(name: Faker::Name.name, email:Faker::Internet.email, manager_id: manager.id)
 end
 
-form1 = Form.create(title: "Bathroom", beacon: 1, manager_id: manager.id)
+form1 = Form.create(title: "Restroom", beacon: 1, manager_id: manager.id)
 form2 = Form.create(title: "Kitchen", beacon: 2, manager_id: manager.id)
 form3 = Form.create(title: "Keg", beacon: 3, manager_id: manager.id)
 
-question2 = Question.create(text: "Are there papertowels?", form_id: form1.id)
-question3 = Question.create(text: "Is there soap?", form_id: form1.id)
-question4 = Question.create(text: "Is everyone wearing shoes?", form_id: form1.id)
 
-question6 = Question.create(text: "Is it clean?", form_id: form2.id)
-question7 = Question.create(text: "Are there dirty dishes?", form_id: form2.id)
-question8 = Question.create(text: "Does the trash need to be dumped?", form_id: form2.id)
+# Questions for restroom
+question2 = Question.create(text: "How many bottles of hand soap were used today?", form_id: form1.id)
+question4 = Question.create(text: "How many packages of papertowels were used today?", form_id: form1.id)
+question3 = Question.create(text: "Were there papertowels used on the floor?", form_id: form1.id)
+question4 = Question.create(text: "Was there urine around the urinals?", form_id: form1.id)
+question5 = Question.create(text: "Does the restroom smell bad?", form_id: form1.id)
+question6 = Question.create(text: "Is everyone wearing shoes?", form_id: form1.id)
+question7 = Question.create(text: "Were any toilets clogged?", form_id: form1.id)
+question8 = Question.create(text: "Is there gum on the urinal traps?", form_id: form1.id)
 
-question10 = Question.create(text: "Is there beer?", form_id: form3.id)
+# Questions for Kitchen
+question9 = Question.create(text: "Is the trash can full?", form_id: form2.id)
+question10 = Question.create(text: "Are there dirty dishes in the sink?", form_id: form2.id)
+question11 = Question.create(text: "Is the microwave a mess?", form_id: form2.id)
+question12 = Question.create(text: "How many bottles of dishsoap was used?", form_id: form2.id)
+question13 = Question.create(text: "Do the fridges have ancient food?", form_id: form2.id)
+question14 = Question.create(text: "How many powerball tabs were used?", form_id: form2.id)
+question15 = Question.create(text: "How many dry food bins are being used?", form_id: form2.id)
+
+
+# Questions for Keg
+question16 = Question.create(text: "Is there beer?", form_id: form3.id)
 question11 = Question.create(text: "Is the keg functional?", form_id: form3.id)
+question12 = Question.create(text: "Is the drip pan clean?", form_id: form3.id)
+question12 = Question.create(text: "What is the CO2 pressure reading?", form_id: form3.id)
 question12 = Question.create(text: "What kind of beer is it?", form_id: form3.id)
+question12 = Question.create(text: "Are the hoses in good working order?", form_id: form3.id)
+question12 = Question.create(text: "Are the seals still working properly?", form_id: form3.id)
+question12 = Question.create(text: "How many packages of cups were used?", form_id: form3.id)
 
-completed1 = CompletedForm.create(form_id: form1.id, employee_id: mike.id)
-completed2 = CompletedForm.create(form_id: form2.id, employee_id: matt.id)
-completed3 = CompletedForm.create(form_id: form3.id, employee_id: mike.id)
+# Completed forms for restroom
+15.times do 
+	CompletedForm.create(form_id: form1.id, employee_id: employees.sample.id)
+end
+
+
+# Completed forms for Kitchen
+15.times do 
+	CompletedForm.create(form_id: form2.id, employee_id: employees.sample.id)
+end
+
+# Completed forms for Keg
+15.times do 
+	CompletedForm.create(form_id: form3.id, employee_id: employees.sample.id)
+end
+
 
 Response.create(completed_form_id: completed1.id, question_id: question2.id, text: "Yes")
 Response.create(completed_form_id: completed1.id, question_id: question3.id, text: "Yes")
