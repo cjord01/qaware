44f3e94248c58ff0148a31ddac8739193763bd73
Clayton Jordan <clayjchicago@gmail.com>
2014-09-26 11:37:55 -0500

    association changes so completed forms can be accessed and I no longer have to save a manager id in the completed form ...form

--- app/models/completed_form.rb
+++ app/models/completed_form.rb
@@ -1,6 +1,6 @@
 class CompletedForm < ActiveRecord::Base
   belongs_to :form
-  belongs_to :manager, through: :employees
+  has_one :manager, through: :employees
   belongs_to :employee
   has_many :responses
   has_many :questions, through: :responses
