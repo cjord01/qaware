3d728f37914518a5fda4a9bd48c4766729fbac06
rickydo <g3tdo@yahoo.com>
2014-09-27 10:47:39 -0500

    me practicing with d3

--- app/assets/javascripts/completed_form.js
+++ app/assets/javascripts/completed_form.js
@@ -1,14 +1,19 @@
-var data = [4,6,8,9,15,46,34];
+$(document).ready(function (){
+	var data = [4, 8, 23, 43, 57, 34]
 
-var x = d3.scale.linear()
-	.domain([0,d3.max(data)])
-	.range([0,420]);
+	var width = 420,
+		barHeight = 20;
 
-d3.select(".chart")
-	.selectAll("div")
-		.data(data)
-	.enter().append("div")
-		.style("width", function(d) {
-			return x(d) + "px";
-		})
-		.text(function(d) {return d; });
+	var x = d3.scale.linear()
+		.domain([0, d3.max(data)])
+		.range([0, width]);
+
+	var chart = d3.
+
+	d3.select(".chart")
+		.selectAll("div")
+			.data(data)
+		.enter().append("div")
+			.style("width", function(d) {return x(d) + "px";})
+			.text(function(d) {return d;});
+});
