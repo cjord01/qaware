b5576f87ecd03ab977a2186ae65432d86f8a49dc
Zach Taylor <taylorzr@gmail.com>
2014-09-28 13:49:44 -0500

    Implements basic api data response

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -9,17 +9,15 @@ class ApiController < ApplicationController
     #     }.to_json
     #   end
     # end
-    @forms = {
-      form1: "this is form 1",
-      form2: "this is form 2",
-      form3: "this is form 3"
+    data = {
+      uuid: "FFC26D16-3710-4922-80CB-7E53E92443E4",
+      major: 1,
+      locations: [
+        { name: "Kegerator", minor: 2 },
+        { name: "Kitchen", minor: 3 },
+        { name: "Bathroom", minor: 4 }
+      ]
     }
-    @string = "yippy kai yay motherfather"
-    render :json => @forms.to_json   
- # respond_to do |format|
-    #   format.html 
-    #   format.json { render @string  }
-    #   format.json 
-    # end
+    render :json => data.to_json
   end
 end
