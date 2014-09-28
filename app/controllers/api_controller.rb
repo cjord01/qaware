154f5defcc22fb93e2b73f8ee95affa985855686
Zach Taylor <taylorzr@gmail.com>
2014-09-28 13:58:08 -0500

    Adds urls

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -13,9 +13,9 @@ class ApiController < ApplicationController
       uuid: "FFC26D16-3710-4922-80CB-7E53E92443E4",
       major: 1,
       locations: [
-        { name: "Kegerator", minor: 2 },
-        { name: "Kitchen", minor: 3 },
-        { name: "Bathroom", minor: 4 }
+        { name: "Kegerator", minor: 2, url: "forms/1" },
+        { name: "Kitchen", minor: 3, url: "forms/2" },
+        { name: "Bathroom", minor: 4, url: "forms/3" }
       ]
     }
     render :json => data.to_json
