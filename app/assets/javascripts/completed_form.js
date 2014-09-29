6ef5bec7782aeb54b71daf26a492878c3dcea950
rickydo <g3tdo@yahoo.com>
2014-09-28 19:43:21 -0500

    still needs to be responsive

--- app/assets/javascripts/completed_form.js
+++ app/assets/javascripts/completed_form.js
@@ -1,31 +1,30 @@
-$(function(){
-	function createGraph(data, xLabels, yLabels, title){
+function createGraph(data, xLabels, yLabels, title){
 
 	var data = data;
 	var xLabels = xLabels;
 	var yLabels = yLabels;
 	var title = title;
 
-	var m = [80, 80, 80, 250]; // margins
-	var w = 700 - m[1] - m[3]; // width: margin.right - margin.left
-	var h = 400 - m[0] - m[2]; // height: margin.top - margin.bottom
+	var m = [80, 80, 80, 250]; 		// margins
+	var width = 700 - m[1] - m[3]; 	// width: margin.right - margin.left
+	var height = 450 - m[0] - m[2]; // height: margin.top - margin.bottom
 		
 	var x = d3.scale.linear()			// by default will create a domain [0,1] and range [1,0]
 		.domain([0, data.length-1])		// customize our domain or how many data points on x-axis
-		.range([0, w]); 				// how wide the x-axis stretches
+		.range([0, width]); 				// how wide the x-axis stretches
 	var y = d3.scale.linear()
 		.domain([0, d3.max(data)])  	// d3.max calculates the max value in the data
-		.range([h, 0]); 				// chart's y-axis will scale with data
+		.range([height, 0]); 				// chart's y-axis will scale with data
 
 	var line = d3.svg.line() 			// create a line function to convert data into x and y points
 
 	// assign the X function to plot line
-	.x(function(d,i) { 
+	.x(function createX(d,i) { 
 		// console.log('Plotting X value for data point: ' + d + ' using index: ' + i + ' to be at: ' + x(i) + ' using our xScale.');
 		// return the X coordinate where we want to plot this datapoint
 		return x(i); 
 	})
-	.y(function(d) { 
+	.y(function createY(d) { 
 		// console.log('Plotting Y value for data point: ' + d + ' to be at: ' + y(d) + " using our yScale.");
 		// return the Y coordinate where we want to plot this datapoint
 		return y(d); 
@@ -33,23 +32,29 @@ $(function(){
 
 	// Add an SVG element with the desired dimensions and margin.
 	var graph = d3.select("#graph").append("svg:svg")
-	    .attr("width", w + m[1] + m[3])
-	    .attr("height", h + m[0] + m[2])
+	    .attr("width", width + m[1] + m[3])
+	    .attr("height", height + m[0] + m[2])
 	    .append("svg:g")
 	    .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+	    // .attr("width", '25%')
+	    // .attr("height", '30%')
+	    // .attr('viewBox','0 0 '+Math.min(width,height)+' '+ Math.min(width,height))
+	    // .attr('preserveAspectRatio','xMinYMin')
+	    // .append("g")
+	    // .attr("transform", "translate(" + Math.min(width,height) / 2 + "," + Math.min(width,height) / 2 + ")");
 
 	// create yAxis
 	var xAxis = d3.svg.axis().scale(x)
 					.tickFormat(function(index){
 						return xLabels[index]
 					})
-					.tickSize(-h)
+					.tickSize(-height)
 					.tickSubdivide(true)
 
 	// Add the x-axis.
 	graph.append("svg:g")
 	    .attr("class", "x axis")
-	    .attr("transform", "translate(0," + h + ")")
+	    .attr("transform", "translate(0," + height + ")")
 	    .call(xAxis)
 	    .selectAll("text")
 	    	.attr("dy", "0.3em")
@@ -75,7 +80,7 @@ $(function(){
 
     // Adding the title
     graph.append("text")
-    	.attr ("x", (w/2))
+    	.attr ("x", (width/2))
     	.attr("y", 0 - (m[0]/2))
     	.attr("text-anchor", "middle")
     	.style("font-size", "1.8rem")
@@ -84,7 +89,13 @@ $(function(){
 	
 	
 	graph.append("svg:path").attr("d", line(data));
-	}
+
+}
+
+
+
+$(function(){
+
 
 	createGraph(rest_q1_data, rest_q1_xLabels, rest_q1_yLabels, rest_q1_title);
 	createGraph(rest_q2_data, rest_q2_xLabels, rest_q2_yLabels, rest_q2_title);
