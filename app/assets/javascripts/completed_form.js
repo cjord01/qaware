7384b52161a4538f8a8c1edb84b0166d2b57199a
rickydo <g3tdo@yahoo.com>
2014-09-29 20:14:56 -0500

    finished with polling on completed forms index, have am/pm showing

--- app/assets/javascripts/completed_form.js
+++ app/assets/javascripts/completed_form.js
@@ -5,45 +5,34 @@ function createGraph(data, xLabels, yLabels, title){
 	var yLabels = yLabels;
 	var title = title;
 
-	var m = [80, 80, 80, 320]; 		// margins
-	var width = 750 - m[1] - m[3]; 	// width: margin.right - margin.left
-	var height = 450 - m[0] - m[2]; // height: margin.top - margin.bottom
+	var m = [80, 80, 80, 320]; 		
+	var width = 750 - m[1] - m[3]; 	
+	var height = 450 - m[0] - m[2]; 
 		
-	var x = d3.scale.linear()			// by default will create a domain [0,1] and range [1,0]
-		.domain([0, data.length-1])		// customize our domain or how many data points on x-axis
-		.range([0, width]); 				// how wide the x-axis stretches
+	var x = d3.scale.linear()			
+		.domain([0, data.length-1])		
+		.range([0, width]); 				
 	var y = d3.scale.linear()
-		.domain([0, d3.max(data)])  	// d3.max calculates the max value in the data
-		.range([height, 0]); 				// chart's y-axis will scale with data
+		.domain([0, d3.max(data)])  	
+		.range([height, 0]); 				
 
-	var line = d3.svg.line() 			// create a line function to convert data into x and y points
-
-	// assign the X function to plot line
+	var line = d3.svg.line() 			
 	.x(function createX(d,i) { 
 		// console.log('Plotting X value for data point: ' + d + ' using index: ' + i + ' to be at: ' + x(i) + ' using our xScale.');
-		// return the X coordinate where we want to plot this datapoint
 		return x(i); 
 	})
 	.y(function createY(d) { 
 		// console.log('Plotting Y value for data point: ' + d + ' to be at: ' + y(d) + " using our yScale.");
-		// return the Y coordinate where we want to plot this datapoint
+	
 		return y(d); 
 	})
 
-	// Add an SVG element with the desired dimensions and margin.
 	var graph = d3.select("#graph").append("svg:svg")
 	    .attr("width", width + m[1] + m[3])
 	    .attr("height", height + m[0] + m[2])
 	    .append("svg:g")
 	    .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
-	    // .attr("width", '25%')
-	    // .attr("height", '30%')
-	    // .attr('viewBox','0 0 '+Math.min(width,height)+' '+ Math.min(width,height))
-	    // .attr('preserveAspectRatio','xMinYMin')
-	    // .append("g")
-	    // .attr("transform", "translate(" + Math.min(width,height) / 2 + "," + Math.min(width,height) / 2 + ")");
-
-	// create yAxis
+	    
 	var xAxis = d3.svg.axis().scale(x)
 					.tickFormat(function(index){
 						return xLabels[index]
@@ -58,7 +47,7 @@ function createGraph(data, xLabels, yLabels, title){
 	    .call(xAxis)
 	    .selectAll("text")
 	    	.attr("dy", "0.3em")
-	    	.attr("dx", "-2em") 			// moves axis labels up or down (negative for down)
+	    	.attr("dx", "-2em") 			
 			.attr("transform", function(d) {
 				return "rotate(-65)"
 			});
@@ -92,18 +81,6 @@ function createGraph(data, xLabels, yLabels, title){
 
 }
 
-// function updateCompletedForms(){
-// 	$.getScript("/completed_forms.js")
-// 	// $.ajax({
-// 	// 	url: '/completed_forms',
-// 	// 	dataType: "html",
-// 	// 	success: success
-// 	// });
-
-// 	// $("completed_forms").append()
-// 	setTimeout(updateCompletedForms, 3000)							// setInterval can do the same thing as setTimeout but setTimeout will only trigger once just in case request takes more time 
-// }
-
 
 $(function(){
 
@@ -129,11 +106,7 @@ $(function(){
 			var request = $.ajax(url, {
 				method: "GET"
 			});
-					// debugger;
 			request.done(function(response){
-				// $forms = $(response).find("table");
-				// $("table").html($forms);
-				// console.log(response[0])
 				renderNewCompletedForm(response);
 				refresh();
 			});
@@ -143,10 +116,10 @@ $(function(){
 
 	function timeFormat(date){
 		if (date.getHours() > 12){
-			return date.getHours()-12 + ":" + date.getMinutes();
+			return date.getHours()-12 + ":" + date.getMinutes() + "pm";
 		}
 		else {
-			return date.getHours() + ":" + date.getMinutes();
+			return date.getHours() + ":" + date.getMinutes() + "am";
 		}
 	}
 
@@ -166,7 +139,6 @@ $(function(){
 						responses[i].form.title + "</td><td>"+  
 						responses[i].employee.name + "</td><tr>");
 				lastId ++;
-				// console.log(lastId);
 		}
 	}
 
