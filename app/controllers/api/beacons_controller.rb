2946575025bddbc0b559083c8e43da307cf89400
Zach Taylor <taylorzr@gmail.com>
2014-09-30 15:28:46 -0500

    Fixes bug in beacons api. Now finds by minor_id instead of beacon_id

--- app/controllers/api/beacons_controller.rb
+++ app/controllers/api/beacons_controller.rb
@@ -2,13 +2,13 @@ class Api::BeaconsController < ApplicationController
   skip_before_filter  :verify_authenticity_token
 
   def update
-    beacon = Beacon.find(params[:beacon_id])
+    beacon = Beacon.find_by(minor: params[:minor_id])
     beacon.update!(employee_id: params[:employee_id])
     head :ok
   end
 
   def destroy
-    beacon = Beacon.find(params[:beacon_id])
+    beacon = Beacon.find_by(params[:minor_id])
     beacon.update!(employee_id: nil)
     head :ok
   end
@@ -18,7 +18,7 @@ end
 =begin
 curl -XPUT -H "Content-Type: application/json" "localhost:3000/api/beacons" -d '
 {
-  "beacon_id": "1",
+  "minor_id": "1",
   "employee_id": "1"
 }'
 =end
@@ -27,7 +27,25 @@ curl -XPUT -H "Content-Type: application/json" "localhost:3000/api/beacons" -d '
 =begin
 curl -XDELETE -H "Content-Type: application/json" "localhost:3000/api/beacons" -d '
 {
-  "beacon_id": "1",
+  "minor_id": "1",
+  "employee_id": "1"
+}'
+=end
+
+# TEST ENTER REGION on HEROKU
+=begin
+curl -XPUT -H "Content-Type: application/json" "http://qaware.herokuapp.com/api/beacons" -d '
+{
+  "minor_id": "2",
+  "employee_id": "1"
+}'
+=end
+
+# TEST EXIT REGION on HEROKU
+=begin
+curl -XDELETE -H "Content-Type: application/json" "http://qaware.herokuapp.com/api/beacons" -d '
+{
+  "minor_id": "2",
   "employee_id": "1"
 }'
 =end
