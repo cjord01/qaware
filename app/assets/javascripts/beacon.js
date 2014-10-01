0741d5dc7b7e63bd5a9461852e1ef44b01f63937
rickydo <g3tdo@yahoo.com>
2014-09-30 23:48:06 -0500

    i hopefully have both tables polling. I have checked the polling for the data table and it works great. the curl test for location table is nonfunctional or doesn't work

--- app/assets/javascripts/beacon.js
+++ app/assets/javascripts/beacon.js
@@ -3,30 +3,30 @@ $(function (){
 		(function refreshLoad() {
 				setTimeout(function(){
 
-				// var url = "/beacons";
-				// var request = $.ajax(url, {
-				// 	method: "GET"
-				// });
-
-				// request.done(function(response){
-				// 	$(".beacons tbody").replaceWith($(response).find("tbody")[0]);
-				// 	$("table totals").replaceWith($(response).find("tbody")[1]);
-				// 	debugger;
-
-				// 	// debugger ;
-				// 	refreshLoad();
-				// });
-
-				var tableUrl = "/beacons"
-				var tableRequest = $.ajax(tableUrl, {
+				var url = "/beacons";
+				var request = $.ajax(url, {
 					method: "GET"
 				});
 
-				tableRequest.done(function(response){
+				request.done(function(response){
+					$(".beacons tbody").replaceWith($(response).find(".beacons tbody"));
 					$(".totals tbody").replaceWith($(response).find(".totals tbody"));
-						// debugger ;
+					// debugger;
+
+					refreshLoad();
 				});
-						refreshLoad();
+
+				// code below works for polling just the data table
+				// var tableUrl = "/beacons"
+				// var tableRequest = $.ajax(tableUrl, {
+				// 	method: "GET"
+				// });
+
+				// tableRequest.done(function(response){
+				// 	$(".totals tbody").replaceWith($(response).find(".totals tbody"));
+				// 		// debugger ;
+				// });
+						// refreshLoad();
 			}, 2000);
 		})();
 
