39b0d273633ea3423d5314e751d7e13a2ad4fa71
rickydo <g3tdo@yahoo.com>
2014-09-30 23:40:02 -0500

    have polling functionality on data table

--- app/assets/javascripts/beacon.js
+++ app/assets/javascripts/beacon.js
@@ -3,31 +3,31 @@ $(function (){
 		(function refreshLoad() {
 				setTimeout(function(){
 
-				var url = "/beacons";
-				var request = $.ajax(url, {
-					method: "GET"
-				});
-
-				request.done(function(response){
-					$(".beacons tbody").replaceWith($(response).find("tbody")[0]);
-					$("table totals").replaceWith($(response).find("tbody")[1]);
-					// debugger;
-
-					// debugger ;
-					refreshLoad();
-				});
-
-				// var tableUrl = "/completed_forms"
-				// var tableRequest = $.ajax(tableUrl, {
+				// var url = "/beacons";
+				// var request = $.ajax(url, {
 				// 	method: "GET"
 				// });
 
-				// tableRequest.done(function(response){
-				// 	$(".data_totals").replaceWith($(response).find(".data_totals"));
+				// request.done(function(response){
+				// 	$(".beacons tbody").replaceWith($(response).find("tbody")[0]);
+				// 	$("table totals").replaceWith($(response).find("tbody")[1]);
+				// 	debugger;
+
+				// 	// debugger ;
+				// 	refreshLoad();
 				// });
-				// 		debugger ;
-				// 		refreshLoad();
-			}, 1000);
+
+				var tableUrl = "/beacons"
+				var tableRequest = $.ajax(tableUrl, {
+					method: "GET"
+				});
+
+				tableRequest.done(function(response){
+					$(".totals tbody").replaceWith($(response).find(".totals tbody"));
+						// debugger ;
+				});
+						refreshLoad();
+			}, 2000);
 		})();
 
 
